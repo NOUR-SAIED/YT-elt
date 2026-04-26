92a6d0fe9d9bcfc0b71708f844c8d968d6b16f75
NOUR-SAIED <nourelhouda.saied@enetcom.u-sfax.tn>
2026-04-26 13:34:05 +0100

    committing before including CI CD pipeline

--- dags/main.py
+++ dags/main.py
@@ -4,6 +4,7 @@ from api.video_stats import get_playlist_id, get_video_ids, extract_video_data ,
 from datetime import datetime, timedelta
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 # Define the local timezone
 local_tz = pendulum.timezone("Africa/Tunis")
@@ -33,38 +34,49 @@ with DAG (
     description="A DAG to produce json",
     schedule="0 14 * * *",
     catchup=False #not to catch up on past runs when the DAG is first created or when it is turned on after being off for a while.
-) as dag:
+) as dag_produce:
     #define tasks 
     playlist_id=get_playlist_id()
     video_ids=get_video_ids(playlist_id)
     extract_data=extract_video_data(video_ids)
     save_to_json=save_data_to_json(extract_data)
     
+    trigger_update_db=TriggerDagRunOperator( # TriggerDagRunOperator is an Airflow operator that allows you to trigger another DAG from within a DAG. In this case, we are using it to trigger the "update_db" DAG after the "produce_json" DAG has completed its tasks.
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+        )
+    
     #dependency between tasks
-    playlist_id >> video_ids >> extract_data >> save_to_json
+    playlist_id >> video_ids >> extract_data >> save_to_json >> trigger_update_db
     
     
 with DAG (
     dag_id="update_db",
     default_args=default_args,
     description="Dag to process JSON file and insert data into both staging an dcore table",
-    schedule="0 15 * * *",
+    schedule=None, # we set the schedule to None because we want this DAG to be triggered by the "produce_json" DAG after it has completed its tasks, rather than running on a regular schedule.
     catchup=False #not to catch up on past runs when the DAG is first created or when it is turned on after being off for a while.
-) as dag:
+) as dag_update:
+    
     #define tasks 
     update_staging=staging_table()
     update_core=core_table()
     
+    trigger_data_quality_checks=TriggerDagRunOperator( # TriggerDagRunOperator is an Airflow operator that allows you to trigger another DAG from within a DAG. In this case, we are using it to trigger the "data_quality_checks" DAG after the "update_db" DAG has completed its tasks.
+        task_id="trigger_data_quality_checks",
+        trigger_dag_id="data_quality_checks",
+        )
+    
     #dependency between tasks
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality_checks
     
 with DAG (
     dag_id="data_quality_checks",
     default_args=default_args,
     description="Dag to perform data quality checks using SODA",
-    schedule="0 16 * * *",
+    schedule=None, # we set the schedule to None because we want this DAG to be triggered by the "update_db" DAG after it has completed its tasks, rather than running on a regular schedule.
     catchup=False #not to catch up on past runs when the DAG is first created or when it is turned on after being off for a while.
-) as dag:
+) as dag_quality:
         
     #define tasks 
     soda_validate_staging=yt_elt_data_quality(staging_schema)
